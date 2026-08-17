62db21db4c4326e0e05d0ee58fbb7d8687d81cd9
ragnarokbaru2019-ops <ragnarokbaru2019@gmail.com>
2026-08-17 17:26:28 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const products = {
         name: "Bakso Urat",
         description: "Bakso urat sapi yang lezat",
         price: 4,
-        image: "images/Bakso Komplit - Kecil.jpg",
+        image: "images/bakso-urat.jpg",
         category: "bakso"
     },
 
@@ -35,7 +35,7 @@ const products = {
         name: "Bakso Telur",
         description: "Bakso dengan isian telur",
         price: 4,
-        image: "images/Bakso Komplit - Mercon.jpg",
+        image: "images/bakso-telur.jpg",
         category: "bakso"
     },
 
@@ -43,7 +43,7 @@ const products = {
         name: "Mie Ayam",
         description: "Mie ayam gurih dan nikmat",
         price: 3,
-        image: "images/Bakso Komplit - Telur.jpg",
+        image: "images/mie-ayam.jpg",
         category: "mie"
     }
 
