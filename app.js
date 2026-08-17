cc9829dd66772a0fc616eecbc47e74b295178217
ragnarokbaru2019-ops <ragnarokbaru2019@gmail.com>
2026-08-17 10:19:09 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const tg =
 
 // MASUKKAN URL /order KAMU YANG SEKARANG
 const ORDER_API_URL =
-    "https://responsibility-channels-lots-importantly.trycloudflare.com/order";
+    "https://jake-athletic-valium-custody.trycloudflare.com/order";
 
 // =========================================================
 // PRODUCTS
