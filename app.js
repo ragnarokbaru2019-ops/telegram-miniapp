11d2e8fec0b5b9214c66f1804284902ecc99ea8b
ragnarokbaru2019-ops <ragnarokbaru2019@gmail.com>
2026-08-19 14:25:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2431,7 +2431,7 @@ async function checkServerStatus() {
         ) {
 
             statusElement.textContent =
-                "🟢 ONLINE";
+                "🟢 OPEN";
 
 
             statusWrapper.className =
@@ -2456,7 +2456,7 @@ async function checkServerStatus() {
     catch (error) {
 
         statusElement.textContent =
-            "🔴 OFFLINE";
+            "🔴 CLOSE";
 
 
         statusWrapper.className =
